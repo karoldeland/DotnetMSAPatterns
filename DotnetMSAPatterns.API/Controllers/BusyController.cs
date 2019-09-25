9ff7210fe00b2ff8c320e3076201d26a9c37379c
Karol <karol.deland@ia.ca>
2019-09-24 22:25:01 -0400

    Initial commit

---
+++ DotnetMSAPatterns.API/Controllers/BusyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotnetMSAPatterns.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class BusyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            bool mustFail = GetFailureFlag();
+
+            if (mustFail)
+                return StatusCode(500, "Fail");
+
+            return Ok("Success");
+        }
+
+        private bool GetFailureFlag()
+        {
+            var seconds = DateTime.Now.Second;
+            var roundedToNearest5seconds = Math.Round(seconds / 5.0) * 5;
+
+            return roundedToNearest5seconds % 2 > 0;
+        }
+    }
+}
---
+++ DotnetMSAPatterns.API/Controllers/FlakyStringController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotnetMSAPatterns.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class FlakyStringController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            bool mustFail = GetFailureFlag();
+                        
+            if (mustFail)
+                return StatusCode(500, "Fail");
+
+            return Ok("Success");
+        }
+
+        private bool GetFailureFlag()
+        {
+            var second = DateTime.Now.Second;
+
+            return second % 2 > 0;
+        }
+    }
+}
