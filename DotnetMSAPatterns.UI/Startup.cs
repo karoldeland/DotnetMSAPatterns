6e8f88e03cb391f5a8a299a89134414d75251571
Karol <karol.deland@ia.ca>
2019-09-30 21:42:47 -0400

    Added samples: Retry + Circuit Breaker using Polly

--- DotnetMSAPatterns.UI/Startup.cs
+++ DotnetMSAPatterns.UI/Startup.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using Polly;
+using Microsoft.AspNetCore.Blazor.Http;
 
 namespace DotnetMSAPatterns.UI
 {
@@ -7,6 +10,28 @@ namespace DotnetMSAPatterns.UI
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<WebAssemblyHttpMessageHandler>();
+
+            services.AddHttpClient("Retry", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/api/");                
+            })
+            .ConfigurePrimaryHttpMessageHandler<WebAssemblyHttpMessageHandler>()
+            .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+            {
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(10)
+            }));
+
+
+            services.AddHttpClient("CircuitBreaker", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/api/");
+            })
+            .ConfigurePrimaryHttpMessageHandler<WebAssemblyHttpMessageHandler>()
+            .AddTransientHttpErrorPolicy(
+                builder => builder.CircuitBreakerAsync(2, TimeSpan.FromSeconds(5)));
         }
 
         public void Configure(IComponentsApplicationBuilder app)
